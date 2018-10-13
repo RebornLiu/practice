4bda1063e4e86da980eebb332d67eed604158e94
Reborn <1025474617@qq.com>
2018-10-13 18:35:56 +0800

    lock test

---
+++ Base/src/test/java/concurrent/LockImplTest.java
@@ -0,0 +1,85 @@
+package concurrent;
+
+import org.junit.Test;
+
+import java.rmi.AccessException;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
+
+public class LockImplTest {
+
+    @Test
+    public void lock() throws InterruptedException {
+
+        //SelfLock lock = new LockImpl();
+
+        SelfLock lock = new RetrenLockImpl();
+        Thread thread1 = new Thread(() -> {
+            try {
+                lock.lock();
+                System.out.println("thread1 get lock");
+                TimeUnit.SECONDS.sleep(5);
+                System.out.println("thread1 try lock again");
+                lock.lock();
+            } catch (InterruptedException e) {
+                System.out.println("intrupt");
+            }
+            finally {
+                System.out.println("thread1 unlock");
+                lock.unlock();
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            try {
+                lock.lock();
+                System.out.println("thread2 get lock");
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                System.out.println("thread 2 intrrupt");
+            }
+            finally {
+                System.out.println("thread2 unlock");
+                lock.unlock();
+            }
+        });
+
+        thread1.setName("self-thread1");
+        thread2.setName("self-thread2");
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+    }
+
+    @Test
+    public void testUtil() throws InterruptedException {
+        Object lock = new Object();
+
+        Thread thread1 = new Thread(() -> {
+           synchronized (lock) {
+               try {
+                   TimeUnit.SECONDS.sleep(1000000);
+               } catch (InterruptedException e) {
+                   e.printStackTrace();
+               }
+           }
+        }, "self-thread1");
+
+
+        Thread thread2 = new Thread(() -> {
+           synchronized (lock) {
+               System.out.println("thread2");
+           }
+        }, "self-thread2");
+
+        thread1.start();
+        TimeUnit.SECONDS.sleep(1);
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+    }
+}
---
+++ Base/src/test/java/concurrent/RWLockTest.java
@@ -0,0 +1,92 @@
+package concurrent;
+
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
+
+public class RWLockTest {
+
+    @Test
+    public void testRWLock() throws InterruptedException {
+
+        RWLock rwLock = new RWLock();
+        RWLock.RLock rLock = rwLock.readLock();
+        RWLock.WLock wLock = rwLock.writeLock();
+
+        Thread read1 = new Thread(() -> {
+            try {
+                rLock.lock();
+                System.out.println("read-thread-1 read lock");
+                TimeUnit.SECONDS.sleep(20);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            finally {
+                rLock.unlock();
+            }
+        }, "read-thread-1");
+
+        Thread read2 = new Thread(() -> {
+            try {
+                rLock.lock();
+                System.out.println("read-thread-2 read lock");
+                TimeUnit.SECONDS.sleep(20);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            finally {
+                rLock.unlock();
+            }
+
+        }, "read-thread-2");
+
+        Thread write1 = new Thread(() -> {
+            try {
+                wLock.lock();
+                System.out.println("write-thread-1 write lock");
+                TimeUnit.SECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            finally {
+                wLock.unlock();
+            }
+        }, "write-thread-1");
+
+        Thread write2 = new Thread(() -> {
+            try {
+                wLock.lock();
+                System.out.println("write-thread-2 write lock");
+                TimeUnit.SECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            finally {
+                wLock.unlock();
+            }
+        }, "write-thread-2");
+
+        read1.start();
+        TimeUnit.MILLISECONDS.sleep(10);
+        read2.start();
+        TimeUnit.MILLISECONDS.sleep(10);
+        write1.start();
+        write2.start();
+
+
+/*        write1.start();
+        TimeUnit.MILLISECONDS.sleep(10);
+        write2.start();
+        TimeUnit.MILLISECONDS.sleep(10);
+        read1.start();
+        read2.start();*/
+
+        read1.join();
+        read2.join();
+        write1.join();
+        write2.join();
+    }
+
+}
