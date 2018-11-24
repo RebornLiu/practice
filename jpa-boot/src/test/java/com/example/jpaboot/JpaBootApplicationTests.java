f5c9d1ef4d77b5163fe5b221cd0f4143782ab9ba
Reborn <1025474617@qq.com>
2018-11-24 20:53:33 +0800

    spring boot jpa 将；连接池设置为不同的profile

--- jpa-boot/src/test/java/com/example/jpaboot/JpaBootApplicationTests.java
+++ jpa-boot/src/test/java/com/example/jpaboot/JpaBootApplicationTests.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
@@ -14,6 +16,8 @@ import javax.persistence.Table;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("hikari")
+//@ActiveProfiles("tomcat")
 public class JpaBootApplicationTests {
 
     @Autowired
