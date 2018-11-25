661f4d485394c117a78385fa5e2d6bb9f24f2333
Reborn <1025474617@qq.com>
2018-11-25 13:25:16 +0800

    spring boot 修改自定义yml的sourcefactory

--- yml-boot/src/main/java/com/example/ymlboot/config/YmlFactory.java
+++ yml-boot/src/main/java/com/example/ymlboot/config/YmlFactory.java
@@ -2,21 +2,24 @@ package com.example.ymlboot.config;
 
 import org.springframework.boot.env.YamlPropertySourceLoader;
 import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.support.DefaultPropertySourceFactory;
 import org.springframework.core.io.support.EncodedResource;
-import org.springframework.core.io.support.PropertySourceFactory;
 
 import java.io.IOException;
 import java.util.List;
 
-public class YmlFactory implements PropertySourceFactory {
+public class YmlFactory extends DefaultPropertySourceFactory {
     @Override
     public PropertySource<?> createPropertySource(String name, EncodedResource resource) throws IOException {
+        if (resource == null) {
+            super.createPropertySource(name, resource);
+        }
         YamlPropertySourceLoader yamlPropertySourceLoader = new YamlPropertySourceLoader();
         List<PropertySource<?>> propertySourceList = yamlPropertySourceLoader.load(name, resource.getResource());
         if (propertySourceList != null && propertySourceList.size() > 0) {
             return propertySourceList.get(0);
         }
 
-        return null;
+        return super.createPropertySource(name, resource);
     }
 }
